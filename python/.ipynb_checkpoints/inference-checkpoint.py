e17e793f51c65ee9ce0455322840e445111b98ae
deagwon97 <curiosity806@naver.com>
2021-02-17 14:47:32 +0900

    update

--- python/.ipynb_checkpoints/inference-checkpoint.py
+++ python/.ipynb_checkpoints/inference-checkpoint.py
@@ -8,17 +8,54 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import re
+from skimage.transform import resize
+from skimage import measure
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import warnings
 
-margin = 70
-device = torch.device('cpu')
+warnings.filterwarnings(action='ignore')
 
+margin = 50
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#device = torch.device('cpu')
+
+def normalize_uint(image):
+    # 입력 받은 numpy array를 0.3 quantile과 0.99 quantile을 기준으로 min max scaleing
+    min_val = np.quantile(image.reshape(-1), 0.3)
+    max_val = np.quantile(image.reshape(-1), 0.99)
+    # 1차 threshold 적용                   
+    over_idx  = (image > max_val)
+    under_idx = (image < min_val)
+    image[over_idx]  = max_val
+    image[under_idx] = min_val
+    normalized = (image - min_val) / (max_val - min_val) * 255    
+    return normalized.astype(np.uint8)
+
+def normalize_float(image):
+    max_val = image.max()
+    min_val = image.min()
+    normalized = (image - min_val) / (max_val - min_val)
+    return normalized
+
+def apply_margin(image, margin):
+    if margin == 0:
+        return image
+    else:
+        # 입력 받은 이미지의 상하좌우로 margin을 적용하고
+        # 원본 크기로 resize하여 반환
+        image_margin = np.zeros(image.shape)
+        for z in range(image.shape[0]):
+            sample = image[z, margin:-margin,margin:-margin].copy()
+            image_margin[z, :,:] = cv2.resize(sample, (512, 512))
+        return image_margin
+    
 def export_num(string):
     # 파일이름에서 숫자를 추출
     # ex) "00006021_converted_138.dcm" -> 138
     find_num = re.compile('[0-9]+')
     number = find_num.findall(string[-10:])
     return int(number[0])
-
 
 def load_image(dir_path):
     # dir_path에 들어있는 dcm파일을 읽어서 3d numpy array로 반환
@@ -35,41 +72,26 @@ def load_image(dir_path):
     for z, image_2d_path in enumerate(dcm_list):
         image_2d = np.array(pydicom.dcmread(dir_path + '/' + file_df.iloc[z,0]).pixel_array)
         image_3d[:,:,z] = image_2d
-    return image_3d
-
-def normalize(image):
-    # 입력 받은 numpy array를 0.3 quantile과 0.99 quantile을 기준으로 min max scaleing
-    min_val = np.quantile(image.reshape(-1), 0.3)
-    max_val = np.quantile(image.reshape(-1), 0.99)
-    # 1차 threshold 적용                   
-    over_idx  = (image > max_val)
-    under_idx = (image < min_val)
-    image[over_idx]  = max_val
-    image[under_idx] = min_val
-    normalized_3d = (image - min_val) / (max_val - min_val)# * 255    
-    #normalized_3d = normalized_3d.astype(np.uint8)#[..., np.newaxis]
-    return normalized_3d
-
-def apply_margin(image, margin):
-    # 입력 받은 이미지의 상하좌우로 margin을 적용하고
-    # 원본 크기로 resize하여 반환
-    image_margin = np.zeros([512, 512, image.shape[2]])
-    for z in range(image.shape[2]):
-        sample = image[margin:-margin,margin:-margin, z].copy()
-        sample = sample.astype(np.uint8)
-        image_margin[:,:,z] = cv2.resize(sample, (512, 512))
-    return image_margin    
+        
+    start = pydicom.dcmread(dir_path + '/' + file_df.iloc[0,0])
+    end = pydicom.dcmread(dir_path + '/' + file_df.iloc[-1,0])
+    start_loc = start.ImagePositionPatient[-1]
+    end_loc = end.ImagePositionPatient[-1]
+    z_length = start_loc - end_loc
+    z_unit = z_length / image_3d.shape[2]
+    xy_unit  = float(start.PixelSpacing[0])
+    return image_3d, xy_unit, z_unit
 
 def predict(image, model):
     # 3차원 입력을 받아서 추론하고 출력 [샘플 수, 세로축, 가로축]
     #image = image[..., np.newaxis]
     image = image.astype('float32')[:,np.newaxis,...]#[샘플수, 채널 수(1), 세로축, 가로축]
-    image_tensor = torch.FloatTensor(image).to(device)
-    predict_mask = model.predict(image_tensor).numpy()
-    return predict_mask
+    image_tensor = torch.cuda.FloatTensor(image).to(device)
+    predict_mask = model.predict(image_tensor).cpu().numpy()
+    return predict_mask    
+    
 
-
-def gen_padding(image, mul = 4):
+def pad_sagittal(image, mul = 4):
     # saggital plane(2차원)을 입력으로 받음
     # 입력받은 이미지의 z축을 4배로 resize
     # 가로 세로 비율을 유지하면서 z축을 512로 resize
@@ -110,15 +132,18 @@ def mask_to_loc(sagittal_mask):
     l3_loc = int(np.median(np.argwhere(col_index)))
     return l3_loc
 
-def mask_to_area(l3_mask, meta_data):
+
+def mask_to_area(l3_mask, xy_unit):
     musle = (l3_mask == 0)
     visceral = (l3_mask == 1)
     subcutaneous = (l3_mask == 2)
-
-    abdomen = musle + visceral + subcutaneous
-    abdomen_img = abdomen.copy().astype(np.uint8)
-    contours, _ = cv2.findContours(abdomen_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
+    abdomen = l3_mask != 3
+    
+    kernel = np.ones((20, 20), np.uint8)
+    abdomen = cv2.dilate(abdomen.astype(np.uint8), kernel, iterations=1)  #// make dilation image
+    abdomen = cv2.erode(abdomen.astype(np.uint8), kernel, iterations=1)  #// make erosion image
+    
+    contours, _ = cv2.findContours(abdomen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     max_area = 0
     for cont in contours:
         area = cv2.contourArea(cont)
@@ -127,23 +152,91 @@ def mask_to_area(l3_mask, meta_data):
             max_contour = cont
 
     abdomen_area = max_area
-    musle_ratio = musle.sum() / abdomen_area
-    visceral_ratio = visceral.sum() / abdomen_area
-    subcutaneous_ratio = subcutaneous.sum() / abdomen_area
+    musle_area = musle.sum()
+    visceral_area = visceral.sum()
+    subcutaneous_area = subcutaneous.sum()
+    other_area = abdomen_area - musle_area\
+                                -visceral_area\
+                                    -subcutaneous_area
+    
+    slice_df = pd.DataFrame(np.zeros((4,1)),
+                        columns = ['area[mm^2]'],
+                        index = ['muscle',
+                                 'visceral',
+                                 'subcutaneous',
+                                 'other'])     
+    
+    slice_df.loc['muscle', 'area[mm^2]'] = musle_area * (xy_unit**2)
+    slice_df.loc['visceral', 'area[mm^2]'] = visceral_area * (xy_unit**2)
+    slice_df.loc['subcutaneous', 'area[mm^2]'] = subcutaneous_area * (xy_unit**2)
+    slice_df.loc['other', 'area[mm^2]'] = other_area * (xy_unit**2)
+    return slice_df
 
-    other_ratio = 1 - musle_ratio\
-                    - visceral_ratio\
-                    - subcutaneous_ratio    
-    meta_data.loc['muscle', 'ratio'] = musle_ratio
-    meta_data.loc['visceral', 'ratio'] = visceral_ratio
-    meta_data.loc['subcutaneous', 'ratio'] = subcutaneous_ratio
-    meta_data.loc['other', 'ratio'] = other_ratio
-    return meta_data
+def slice_to_vol(slice_list, z_unit):
+    vol_array = np.array(slice_list)    
+    vol = vol_array.sum(axis = 0).reshape(-1,1) * z_unit
+    vol_df = pd.DataFrame(vol,
+                            columns = ['volume[mm^3]'],
+                            index = ['muscle',
+                                     'visceral',
+                                     'subcutaneous',
+                                     'other'])
+    return vol_df
 
 
+def draw_3d_plot(image,
+                 visceral_3d,
+                 muscle_3d,
+                 subcutaneous_3d):
+    
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    alpha = 0.1
+    image_p = image.transpose(2,1,0)
+
+    for threshold in np.arange(0.4, 0.7, 0.1):
+        alpha = alpha * 0.5
+        verts, faces, normals, values = measure.marching_cubes_lewiner(image_p, threshold)
+        mesh = Poly3DCollection(verts[faces], alpha=alpha)
+        face_color = [0, 0, 0]
+        mesh.set_facecolor(face_color)
+        ax.add_collection3d(mesh)
+
+    visceral_p = visceral_3d.transpose(2,1,0)
+    verts, faces, normals, values = measure.marching_cubes_lewiner(visceral_p, 0.5)
+    mesh = Poly3DCollection(verts[faces], alpha=1)
+    face_color = [1, 0, 0]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    muscle_p = muscle_3d.transpose(2,1,0)
+    verts, faces, normals, values = measure.marching_cubes_lewiner(muscle_p, 0.5)
+    mesh = Poly3DCollection(verts[faces], alpha=0.3)
+    face_color = [0, 0.8, 0]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    subcutaneous_p = subcutaneous_3d.transpose(2,1,0)
+    verts, faces, normals, values = measure.marching_cubes_lewiner(subcutaneous_p, 0.5)
+    mesh = Poly3DCollection(verts[faces], alpha=0.2)
+    face_color = [0, 0, 0.5]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+
+    ax.set_xlim(0, image_p.shape[0])
+    ax.set_ylim(0, image_p.shape[1])
+    ax.set_zlim(0, image_p.shape[2])
+
+    ax.axis('off')
+    fig = plt.gcf()
+    fig.savefig(output_dir + "/" + 'predict_3d.png',
+                bbox_inches = 'tight', pad_inches=0)
+    
 
 if __name__ == "__main__":
-
+    
     # input path
     if sys.argv[1] == '-input_dir':
         input_dir = sys.argv[2]
@@ -151,31 +244,32 @@ if __name__ == "__main__":
         output_dir = sys.argv[4]
     if sys.argv[5] == '-models_dir':
         models_dir = sys.argv[6]
-    if sys.argv[7] == '-window_size':
-        window_size = int(sys.argv[8])
-        
+    if sys.argv[7] == '-z_length':
+        z_length = int(sys.argv[8])
 
     with torch.no_grad():    
         #모델 불러오기-------------------------------------------------------------
         # abdomen영역을 detect 모델
-        detect_model = torch.load(models_dir  + "/" + "detect_model.pth").to(device)
+        detect_model = torch.load(models_dir  + "/" + "detect_model.pth", device)
         # abdomen에서 musle, visceral, subcutaneous를 segment 모델
-        segment_model = torch.load(models_dir + "/" + "segment_model.pth").to(device)
+        segment_model = torch.load(models_dir + "/" + "segment_model.pth", device)
 
 
         #3차원 이미지 불러오기 불러오기--------------------------------------------
-        image_3d = load_image(input_dir)
+        image_3d, xy_unit, z_unit = load_image(input_dir)
+        window_size = int(z_length // z_unit // 2)
 
         # detect abdomen
         sagittal = image_3d[:,256,:]
-        sagittal_normal = normalize(sagittal)
-        sagittal_padding = gen_padding(sagittal_normal)[np.newaxis, ...]
+        sagittal_normal = normalize_uint(sagittal)
+        sagittal_normal = normalize_float(sagittal_normal)
+        sagittal_padding = pad_sagittal(sagittal_normal)[np.newaxis, ...]
         sagittal_mask = predict(sagittal_padding, detect_model)[0,0,...]
 
         l3_loc_padding = mask_to_loc(sagittal_mask) #추가 정의 필요
         l3_loc = int(sagittal.shape[1] * (l3_loc_padding / sagittal.shape[0]))
 
-
+        # draw sagittal plane plots
         plt.figure(figsize = (10,10))
         plt.imshow(sagittal_padding[0,...])
         ax = plt.gca()
@@ -183,8 +277,8 @@ if __name__ == "__main__":
         fig = plt.gcf()
         fig.savefig(output_dir + "/" + 'sagittal.png',
                     bbox_inches='tight', pad_inches=0)
-        
-        
+
+
         plt.figure(figsize = (10,10))
         plt.imshow(sagittal_padding[0,...])
         plt.imshow(sagittal_mask, alpha = 0.2)
@@ -202,14 +296,22 @@ if __name__ == "__main__":
         horizontal_stack = image_3d[:,:,
                                     l3_loc - window_size : l3_loc + window_size]\
                             .transpose(2, 0, 1)
-        #horizontal_stack = apply_margin(horizontal_stack, margin = 70)
         # 정규화
-        horizontal_normal  = normalize(horizontal_stack)
+        horizontal_normal  = normalize_uint(horizontal_stack)
+
+        # 마진 적용
+        horizontal_normal = apply_margin(horizontal_normal, margin = margin)
+        margin_ratio = (512 - margin) / (512)
+
+        # minmax
+        horizontal_normal  = normalize_float(horizontal_normal)
+
         # 예측
         sagittal_mask = predict(horizontal_normal, segment_model)#[0,0,...]
         horizontal_mask = sagittal_mask.transpose([0, 2, 3, 1]).argmax(axis = 3)
         horizontal_mask = (horizontal_mask * 255 / 3).astype(np.uint8)
 
+        # horizontal 원본
         plt.figure(figsize = (10,10))
         plt.imshow(horizontal_normal[window_size,...], cmap = 'bone')
         ax = plt.gca()
@@ -218,6 +320,7 @@ if __name__ == "__main__":
         fig.savefig(output_dir + "/" + 'horizontal.png',
                     bbox_inches='tight', pad_inches=0)
 
+        # horizontal 전부 중복
         plt.figure(figsize = (10,10))
         plt.imshow(horizontal_normal[window_size,...], cmap = 'bone')
         plt.imshow(horizontal_mask[window_size,...], alpha = 0.2, cmap = 'flag')
@@ -226,15 +329,84 @@ if __name__ == "__main__":
         fig = plt.gcf()
         fig.savefig(output_dir + "/" + 'horizontal_overray.png',
                     bbox_inches='tight', pad_inches=0)
-        #--------------------------------------------------------------------------    
-        meta_data = pd.DataFrame([0., 0., 0., 0.],
-                                 columns = ['ratio'])
-        meta_data.index = ['muscle', 'visceral','subcutaneous', 'other']
-        ratios = []
+
+        # horizontal muscle 중복
+        plt.figure(figsize = (10,10))
+        plt.imshow(horizontal_normal[window_size,...], cmap = 'bone')
+        muscle = horizontal_mask[window_size,...]/255*3 != 0
+        plt.imshow(muscle.astype(np.uint8)*255, alpha = 0.2, cmap = 'flag')
+        ax = plt.gca()
+        ax.axis('off')
+        fig = plt.gcf()
+        fig.savefig(output_dir + "/" + 'muscle_overray.png',
+                    bbox_inches='tight', pad_inches=0)
+
+        # horizontal visceral 중복
+        plt.figure(figsize = (10,10))
+        plt.imshow(horizontal_normal[window_size,...], cmap = 'bone')
+        visceral = horizontal_mask[window_size,...]/255*3 != 1
+        plt.imshow(visceral.astype(np.uint8)*255, alpha = 0.2, cmap = 'flag')
+        ax = plt.gca()
+        ax.axis('off')
+        fig = plt.gcf()
+        fig.savefig(output_dir + "/" + 'visceral_overray.png',
+                    bbox_inches='tight', pad_inches=0)
+
+        # horizontal subcutaneous 중복
+        plt.figure(figsize = (10,10))
+        plt.imshow(horizontal_normal[window_size,...], cmap = 'bone')
+        subcutaneous = horizontal_mask[window_size,...]/255*3 != 2
+        plt.imshow(subcutaneous.astype(np.uint8)*255, alpha = 0.2, cmap = 'flag')
+        ax = plt.gca()
+        ax.axis('off')
+        fig = plt.gcf()
+        fig.savefig(output_dir + "/" + 'subcutaneous_overray.png',
+                    bbox_inches='tight', pad_inches=0)
+        #--------------------------------------------------------------------------
+        slice_list = []
         for z in range(horizontal_mask.shape[0]):
             l3_mask = horizontal_mask[z] / 255 * 3 
-            meta_sample = mask_to_area(l3_mask, meta_data)
-            ratios.append(meta_sample.ratio.values.copy())
+            slice_area = mask_to_area(l3_mask, xy_unit)
+            slice_area = slice_area * margin_ratio**2
+            slice_list.append(slice_area['area[mm^2]'].values.copy())
+            if z == window_size:
+                slice_area.to_csv(output_dir + '/' + "slice_df.csv")
+        vol_df = slice_to_vol(slice_list,z_unit)
+        vol_df.to_csv(output_dir + '/' + "vol_df.csv")
 
-        meta_data['ratio'] = np.vstack(ratios).mean(axis = 0)
-        meta_data.to_csv(output_dir + '/' + "meta_data.csv")
+        #--------------------------------------------------------------------------    
+        start = l3_loc  - horizontal_mask.shape[0] // 2
+        end = start + horizontal_mask.shape[0]
+        predict_mask = np.zeros(image_3d.shape)
+        predict_mask[...] = -1
+        predict_mask[:,:,start:end] = (horizontal_mask / 255 * 3).transpose(1,2,0)
+        np.save(output_dir +"/predict_mask.npy", predict_mask.astype(np.uint8))
+        #--------------------------------------------------------------------------
+
+        # 3차원 plot 그리기 데이터 생성
+        muscle_3d = (predict_mask == 0) * 255
+        visceral_3d = (predict_mask== 1) * 255
+        subcutaneous_3d = (predict_mask == 2) * 255
+
+        # resize
+        image_3d = image_3d.transpose(2,0,1)
+        image_3d_margin = apply_margin(image_3d, margin = margin)
+        image_3d_margin = image_3d_margin.transpose(1,2,0)
+        image = resize(image_3d_margin, (100, 100, 180), mode='constant')
+        muscle_3d = resize(muscle_3d, (100, 100, 180), mode='constant')
+        visceral_3d = resize(visceral_3d, (100, 100, 180), mode='constant')
+        subcutaneous_3d = resize(subcutaneous_3d, (100, 100, 180), mode='constant')
+
+        # normalization
+        image = normalize_float(image)
+        muscle_3d = normalize_float(muscle_3d)
+        visceral_3d = normalize_float(visceral_3d)
+        subcutaneous_3d = normalize_float(subcutaneous_3d)
+
+        # draw plot
+        draw_3d_plot(image,visceral_3d,
+                    muscle_3d,subcutaneous_3d)
+            #--------------------------------------------------------------------------
+        #display(vol_df)
+        #display(slice_area)
+        #--------------------------------------------------------------------------
--- python/README.txt
+++ python/README.txt
@@ -1,17 +1,39 @@
+[수정내용]------------------------------------------------
+ 예측한 결과의 3차원 정보를 predict_3d.png, predict_mask.npy형태로 저장합니다.
+
+참고) slice를 상하 20장으로 했을때 z_length는 약 130mm입니다.
+
+[실행]------------------------------------------------
 inference.py가 들어있는 폴더에서 터미널로 다음과 같이 inference.py를 실행합니다.
 
-python inference.py -input_dir input -output -models_dir models -window_size 2
+python inference.py -input_dir input -output_dir output -models_dir models -z_length 130
 
+[상세]------------------------------------------------
 
 1. inference.py에서는 input 폴더 속에 들어있는 .dcm파일들을 불러와 3차원 이미지를 생성합니다.
 2. 생성된 3차원 이미지를 통해 L3의 좌표를 추론합니다.
 3. 원본 Saggital plane의 이미지(sagittal.png)와 
 4. 추론한 L3좌표가 표시된 이미지(sagittal.overray.png)가 output폴더에 저장됩니다.
 
-5.추론한 L3좌표를 기준으로 상하 window_size 의 horizonttal plane을 생성합니다.
-6. 생성된 window_size * 2개의 horizontal plane의 muscle, visceral, subcutanoues를 예측합니다.
-7. window_size * 2 개의 horizontal plane의 예측값으로 부터 영역별 비율을 계산합니다.
-8. 계산된 비율을 meta_data.csv에 저장합니다.
+5.추론한 L3좌표를 기준으로 상하 z_length/2 길이([mm])의 영역에 해당하는 horizonttal planes을 생성합니다.(N개)
+6. 생성된 N개의 horizontal plane의 muscle, visceral, subcutanoues를 예측합니다.
+7. N 개의 horizontal plane의 예측값으로 부터 영역별 면적을 계산합니다.
+8. 계산된 면적들 중 L3좌표에 해당하는 slice를 slice_df.csv에 저장합니다.
 
-9. 샘플로 L3좌표의 horizontal plane를 horizontal.png로 저장합니다.
-9. 샘플로 L3좌표의 horizontal plane의 예측값을 horizontal_overray.png로 저장합니다.
+9. N 개의 horizontal plane들에 대한 영역별 면적과, dicom에서 읽은 z축의 pixcel unit으로 부터 
+영역별 부피를 계산하여 vol_df.csv에 저장합니다.
+
+
+10. 샘플로 L3좌표의 horizontal plane를 horizontal.png로 저장합니다.
+11. 샘플로 L3좌표의 horizontal plane의 예측값을 horizontal_overray.png로 저장합니다.
+12. 샘플로 L3좌표의 horizontal plane의 예측값을 부위별로 저장합니다.
+
+13. 추론한 L3좌표를 기준으로 상하 z_length/2 길이([mm]) 구간의 영역별 예측결과를 predict_3d.png, predict_mask.npy형태로 저장합니다.
+- predict.npy : (512,512, {dicom slice 수})
+     -1 : 예측하지 않은 영역( L3좌표 상하 z_length/2 길이([mm]) 구간 밖)
+      0 : 근육
+      1 : 내장지방
+      2 : 피하지방
+      3 : 배경
+
+
